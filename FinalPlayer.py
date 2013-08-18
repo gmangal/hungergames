3da0229146cc70f179366436fe3f0416c777599f
Gaurav <gmangal@email.arizona.edu>
2013-08-18 15:00:36 -0700

    Added some documentation to FinalPlayer to describe our logic a little bit

--- FinalPlayer.py
+++ FinalPlayer.py
@@ -1,3 +1,27 @@
+'''
+This player attempts to survive by analyzing the overall returns from hunting
+and slacking each round. The array of decisions made during the round is saved
+and then compared to the array of returns from each interaction that is given in
+the hunt_outcomes method. 
+
+If this player slacked for an interaction, the returns
+(positive or negative) are added to a slackEarnings variable. Similarly, if this
+player hunted for an interaction, the returns are added to a huntEarnings
+variable. 
+
+If the communal food is received due to the number of hunts exceeding
+m, those earnings are added to huntEarnings since that food is earned by players
+hunting
+
+If, for any given round, hunting is overall earning the player more food (or
+losing less) than slacking, this means that more players are hunting. This
+player tries to take advantage of that by attempting to slack more often.
+
+However, if slacking is earning more than hunting, that probably means that
+other players are responding to this player's slack by slacking themselves
+(note: this is more a case of minimizing losses than maximizing returns). Thus,
+this player tries to encourage others to hunt more by opting to hunt more often.
+'''
 class Player():
     # player that adjusts threshold for hunting based on aggreate
     # results of each action for the previous round
@@ -32,8 +56,9 @@ class Player():
     
     def hunt_outcomes(self, food_earnings):
         
+        # total earnings from hunting, reset to 0 for the round
         self.huntEarnings = 0
-        # total earnings from slacking
+        # total earnings from slacking, reset to 0 for the round
         self.slackEarnings = 0
         
         for i in range(0, len(food_earnings)):
